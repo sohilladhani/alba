75d23eb83c650608d63c3d4a6f62d7fd9a3903ab
Sohil Ladhani <supertakumi86@gmail.com>
2020-05-11 17:46:31 +0530

    Add height, aspect ratio and scale

--- src/com/sohilladhani/alba/Game.java
+++ src/com/sohilladhani/alba/Game.java
@@ -2,5 +2,7 @@ package com.sohilladhani.alba;
 
 public class Game {
     public static int width = 300;
-
+    private static int aspectRatio = 16 * 9;
+    public static int height = width / aspectRatio;
+    public static int scale = 3;
 }
