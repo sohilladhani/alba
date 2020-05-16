bbf94309e7bb2c072421419f0a494f4bbac5ec35
Sohil Ladhani <supertakumi86@gmail.com>
2020-05-17 02:40:13 +0530

    Add method to clear screen before rendering pixels
    The Cherno Project - Game Programming Episode 10

--- src/com/sohilladhani/alba/graphics/Screen.java
+++ src/com/sohilladhani/alba/graphics/Screen.java
@@ -1,5 +1,7 @@
 package com.sohilladhani.alba.graphics;
 
+import java.util.Random;
+
 /* Screen class for rendering the pixels with colors we specify */
 public class Screen {
     private int width;
@@ -15,8 +17,14 @@ public class Screen {
     public void render() {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                pixels[x + y * width] = 0x4285F4;
+                pixels[new Random().nextInt(width) + new Random().nextInt(height) * width] = 0x4285F4;
             }
+        }
+    }
+
+    public void clear() {
+        for(int i = 0; i < pixels.length; i++) {
+            pixels[i] = 0;
         }
     }
 }
