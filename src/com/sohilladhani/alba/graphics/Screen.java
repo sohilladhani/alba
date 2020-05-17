87e0a720559c4cde1d577845a01aad3024ea83ea
Sohil Ladhani <supertakumi86@gmail.com>
2020-05-17 13:09:37 +0530

    Update pixels code to remove random variables

--- src/com/sohilladhani/alba/graphics/Screen.java
+++ src/com/sohilladhani/alba/graphics/Screen.java
@@ -8,9 +8,6 @@ public class Screen {
     private int width;
     private int height;
     public int[] pixels;
-    int count = 0;
-    int xtime = 0;
-    int ytime = 0;
 
     public Screen(int width, int height) {
         this.width = width;
@@ -19,15 +16,10 @@ public class Screen {
     }
 
     public void render() {
-        count++;
-        if (count % 10==0) xtime = xtime + new Random().nextInt(10);
-        if (count % 80==0) ytime = ytime + new Random().nextInt(80);
-
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 /* Some random value assigning to pixels */
-                pixels[(ytime + xtime * width) % (width * height)] = 0xF4B400;
-                pixels[(xtime + ytime * width) % (width * height)] = 0x4285F4;
+                pixels[(x + y * width) % (width * height)] = 0x0F9D58;
             }
         }
     }
