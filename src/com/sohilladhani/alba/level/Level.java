d4c9afe91328226d7ef620db59acc9d7c57333e0
Sohil Ladhani <supertakumi86@gmail.com>
2020-05-17 22:58:03 +0530

    Add code to get tiles
    The Cherno Project - Game Programming - Episode 32 - Getting Tiles

--- src/com/sohilladhani/alba/level/Level.java
+++ src/com/sohilladhani/alba/level/Level.java
@@ -1,6 +1,7 @@
 package com.sohilladhani.alba.level;
 
 import com.sohilladhani.alba.graphics.Screen;
+import com.sohilladhani.alba.tiles.Tile;
 
 public class Level {
     protected int width, height;
@@ -40,5 +41,10 @@ public class Level {
         int x1 = (xScroll + screen.width) >> 4;
         int y0 = yScroll >> 4;
         int y1 = (yScroll + screen.height) >> 4;
+    }
+
+    public Tile getTile(int x, int y) {
+        if(tiles[x + y * width] == 0) return Tile.grass;
+        return null;
     }
 }
